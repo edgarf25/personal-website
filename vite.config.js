37519f2199d1daa43ff089f9323dfd3b744d5a1d
Edgar <edgarfong25@hotmail.com>
2024-09-12 04:24:40 -0700

    Added correct path for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',   // The output directory for the build
-    rollupOptions: {
-      input: './index.html', // Ensure the correct input file is used
-    },
-  },
-  server: {
-    open: true,  // Automatically opens the app in the browser
-  },
+  base: '/personal-website/',
 });
