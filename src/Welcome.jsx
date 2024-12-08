dd0fa5da254a514a7b4cff434881d164951dba16
Edgar <edgarfong25@hotmail.com>
2024-12-07 23:04:02 -0800

    Added links to github and linked in, updated resume. Added effects for some buttons

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,5 +1,7 @@
 import './Welcome.css'
 import profile from './assets/me.png'
+import github from './assets/github.png'
+import linkedin from './assets/linkedin.png'
 
 function Welcome() {
   return(
@@ -7,6 +9,16 @@ function Welcome() {
         <img src={profile} alt="profile"/>
         <h1><span>Hello, I&apos;m Edgar Fong </span>  a Software Engineer based in California.</h1>
         <p>I&apos;m a software engineer who loves building and designing outstanding digital experiences. Currently, I&apos;m eager to expand my skill set and take on new, challenging projects as well as new opportunities.</p> 
+
+        <div className="social">
+          <a href="https://github.com/edgarf25" target="_blank" rel="noopener noreferrer">
+            <img src={github} alt="github"/>
+          </a>
+          <a href="https://www.linkedin.com/in/edgar-fong" target="_blank" rel="noopener noreferrer">
+            <img src={linkedin} alt="linkedin"/>
+          </a>
+        </div>
+    
     </div>
 );
 }
